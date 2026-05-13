c382f5c9fd1083093e4f96a8665615bb92e8c44a
taiwithers <59430904+taiwithers@users.noreply.github.com>
2026-05-13 18:02:11 -0400

    Marko cleanup

--- src/cambium/md_transform.py
+++ src/cambium/md_transform.py
@@ -1,9 +1,11 @@
 from marko import Markdown
 from marko.html_renderer import HTMLRenderer
 from marko.inline import Link
+from typing_extensions import override
 
 
 class CambiumHTMLRenderer(HTMLRenderer):
+    @override
     def render_link(self, element: Link) -> str:
         if element.dest.endswith(".md"):
             element.dest = element.dest[: element.dest.rindex(".md")] + ".html"
@@ -11,5 +13,5 @@ class CambiumHTMLRenderer(HTMLRenderer):
 
 
 def markdown_to_html(markdown: str) -> str:
-    # TODO: move the instance declaration out of this function so we don't create a new instance every file
+    # WARNING: The Markdown class is not thread-safe. Create a new instance for each thread.
     return Markdown(extensions=["gfm"], renderer=CambiumHTMLRenderer).convert(markdown)
