656b05508595ac472f811d0d6ca19ecd5a216488
Mubdi Rahman <mubdi@sidratresearch.com>
2026-05-14 16:09:43 -0400

    Updated CLI with configuration

--- src/cambium/cli.py
+++ src/cambium/cli.py
@@ -1,5 +1,6 @@
 import typer
 
+from cambium import config
 from cambium.tree import TreeSpan
 
 app = typer.Typer()
@@ -7,6 +8,9 @@ app = typer.Typer()
 
 @app.command()
 def main():
+
+    config.initialize_configuration()
+
     treespan = TreeSpan()
     treespan.transform()
     treespan.finalize()
