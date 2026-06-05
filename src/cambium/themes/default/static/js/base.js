c7730a1456420f04eb2130b8c33294b5bc83bbee
taiwithers <59430904+taiwithers@users.noreply.github.com>
2026-06-05 14:11:39 -0400

    Add basic hamburger menu

--- src/cambium/themes/default/static/js/base.js
+++ src/cambium/themes/default/static/js/base.js
@@ -1,4 +1,8 @@
 import { addSortingFunctionToAllTables } from "./tableSorting.js";
+import { attachMenuButtonListener } from "./menu.js";
 
 // Adding Sortable Nature to all Tables
 addSortingFunctionToAllTables();
+
+// Adding event listener to menu button
+attachMenuButtonListener();
---
+++ src/cambium/themes/default/static/js/menu.js
@@ -0,0 +1,16 @@
+function onMenuToggle(menuPanel) {
+  menuPanel.toggleAttribute("hidden");
+}
+
+export function attachMenuButtonListener() {
+  const menuButtonOpen = document.getElementById("menu-button-open");
+  const menuButtonClose = document.getElementById("menu-button-close");
+  const menuPanel = document.getElementById("menu-panel");
+
+  menuButtonOpen.addEventListener("click", () => {
+    onMenuToggle(menuPanel);
+  });
+  menuButtonClose.addEventListener("click", () => {
+    onMenuToggle(menuPanel);
+  });
+}
