556af671a976328746073c6cddcbb440573133c5
Mubdi Rahman <mubdi@sidratresearch.com>
2026-06-17 16:11:21 -0400

    Added menu javascript

--- src/cambium/themes/default/static/js/menu.js
+++ src/cambium/themes/default/static/js/menu.js
@@ -1,16 +1,33 @@
-function onMenuToggle(menuPanel) {
-  menuPanel.classList.toggle("hidden");
+function openMenu() {
+  const menuPanel = document.getElementById("menu-panel");
+  menuPanel.classList.remove("hidden");
+  document.addEventListener("scroll", closeMenu);
+  document.addEventListener("click", closeMenuIfClickedOutside);
+}
+
+function closeMenu() {
+  const menuPanel = document.getElementById("menu-panel");
+  menuPanel.classList.add("hidden");
+  document.removeEventListener("scroll", closeMenu);
+  document.removeEventListener("click", closeMenuIfClickedOutside);
+}
+
+function closeMenuIfClickedOutside(event) {
+  const header = document.getElementsByTagName("header")[0];
+  const menuPanel = document.getElementById("menu-panel");
+  if (!menuPanel.contains(event.target) & !header.contains(event.target)) {
+    closeMenu();
+  }
 }
 
 export function attachMenuButtonListener() {
   const menuButtonOpen = document.getElementById("menu-button-open");
   const menuButtonClose = document.getElementById("menu-button-close");
-  const menuPanel = document.getElementById("menu-panel");
 
   menuButtonOpen.addEventListener("click", () => {
-    onMenuToggle(menuPanel);
+    openMenu();
   });
   menuButtonClose.addEventListener("click", () => {
-    onMenuToggle(menuPanel);
+    closeMenu();
   });
 }
