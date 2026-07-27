d6095cb79f3453c0a4039054b790892296ab88b1
taiwithers <59430904+taiwithers@users.noreply.github.com>
2026-07-27 16:37:19 -0400

    Add some tests for build directories

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pytest
 
-from cambium import config
+from cambium import cli, config
 
 
 @pytest.mark.parametrize(
@@ -41,3 +41,50 @@ def test_merge_config(tmp_path: Path) -> None:
     assert str(config.current_config.root_dir) == cli_config["root_directory"]
 
     assert config.current_config.build_dir is not None
+
+
+def test_root_build(tmp_path: Path) -> None:
+    """Tests related to the root and build directories."""
+    config_path = None
+    min_cli_config = {
+        "dev_server": False,
+        "dev_server_port": 8000,
+        "dev_server_interval": 1,
+    }
+    verbosity = 0
+
+    # build directory always returns absolute
+    cli_build = "../../_build/"
+    cli.setup_config(
+        config_path, {"build_directory": cli_build, **min_cli_config}, verbosity
+    )
+    assert config.current_config.build_dir.is_absolute()
+    # when given relative, build should be relative to root
+    expected_build = (config.current_config.root_dir / cli_build).resolve()
+    assert config.current_config.build_dir == expected_build
+
+    # root directory does not exist
+    with pytest.raises(AssertionError):
+        cli.setup_config(
+            config_path,
+            {"root_directory": str(tmp_path / "nonexistent"), **min_cli_config},
+            verbosity,
+        )
+
+    # build directory is the same as unspecified root
+    with pytest.raises(AssertionError):
+        cli.setup_config(
+            config_path, {"build_directory": ".", **min_cli_config}, verbosity
+        )
+
+    # build directory is the same as specified root
+    with pytest.raises(AssertionError):
+        cli.setup_config(
+            config_path,
+            {
+                "root_directory": str(tmp_path),
+                "build_directory": str(tmp_path),
+                **min_cli_config,
+            },
+            verbosity,
+        )
