f420110216d6bae19ed6014bbc9dad039e5da192
taiwithers <59430904+taiwithers@users.noreply.github.com>
2026-08-05 14:06:48 -0400

    Move fn out of utils

--- src/cambium/builtin_stages/url_encode_filenames.py
+++ src/cambium/builtin_stages/url_encode_filenames.py
@@ -1,15 +1,18 @@
 """Cambium stage to ensure filenames are URL safe."""
 
 import logging
+import urllib
 from pathlib import Path
 
 from marko import Markdown
+from marko.element import Element
+from marko.inline import Image, Link
 from marko.md_renderer import MarkdownRenderer
 from slugify import slugify
 
 from ..stage import Stage
 from ..tree import TreeSpan
-from .utils import rewrite_urlsafe_links
+from .utils import is_external_link
 
 logger = logging.getLogger(__name__)
 
@@ -70,3 +73,36 @@ class URLEncodeFilenames(Stage):
         )
 
         leaf_path.write_text(marko_object.render(document))
+
+
+def rewrite_urlsafe_links(
+    element: Element, parent_directory: Path, links_to_update: dict[Path, Path]
+) -> Element:
+    """Replace image and link destinations in a markdown element with versions that are
+    both url and filesystem safe.
+    """
+    if isinstance(element, str):
+        return element
+
+    if isinstance(element, (Link, Image)):
+        if is_external_link(element.dest):
+            return Element
+
+        dest_path = parent_directory / element.dest
+        dest_path_urldecoded = Path(urllib.parse.unquote(str(dest_path)))
+
+        new_dest = element.dest
+
+        if dest_path in links_to_update:
+            new_dest = links_to_update[dest_path]
+        elif dest_path_urldecoded in links_to_update:
+            # dest path was pre-url encoded
+            new_dest = links_to_update[dest_path_urldecoded]
+
+        element.dest = new_dest
+
+        return element
+
+    for child in element.children:
+        child = rewrite_urlsafe_links(child, parent_directory, links_to_update)
+    return element
--- src/cambium/builtin_stages/utils.py
+++ src/cambium/builtin_stages/utils.py
@@ -60,39 +60,6 @@ def is_external_link(dest: str) -> bool:
     return any(dest.startswith(prefix) for prefix in ["http:", "https:", "www."])
 
 
-def rewrite_urlsafe_links(
-    element: Element, parent_directory: Path, links_to_update: dict[Path, Path]
-) -> Element:
-    """Replace image and link destinations in a markdown element with versions that are
-    both url and filesystem safe.
-    """
-    if isinstance(element, str):
-        return element
-
-    if isinstance(element, (Link, Image)):
-        if is_external_link(element.dest):
-            return Element
-
-        dest_path = parent_directory / element.dest
-        dest_path_urldecoded = Path(urllib.parse.unquote(str(dest_path)))
-
-        new_dest = element.dest
-
-        if dest_path in links_to_update:
-            new_dest = links_to_update[dest_path]
-        elif dest_path_urldecoded in links_to_update:
-            # dest path was pre-url encoded
-            new_dest = links_to_update[dest_path_urldecoded]
-
-        element.dest = new_dest
-
-        return element
-
-    for child in element.children:
-        child = rewrite_urlsafe_links(child, parent_directory, links_to_update)
-    return element
-
-
 def resolve_internal_link(
     link: Path, parent_directory: Path, build_directory: Path
 ) -> Path:
