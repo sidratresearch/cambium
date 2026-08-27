sidratresearch/cambium

commit c44b7189ad06cb5f2bd41cf8f0a75cc8d4529a30

fix: ensure nice cleanup of dev server files for any exception type

src/cambium/cli/dev_server.py
@@ -63,8 +63,9 @@ def run_dev_server(
                     tree = TreeSpan(config.current_config)  # make new tree
                     logger.info("Re-running Cambium")
                     build(tree)
-    except KeyboardInterrupt:
-        # TODO: how does this behave for other exceptions?
+    except Exception as e:
+        if not isinstance(e, KeyboardInterrupt):
+            logger.error(e)
         logger.info("Closing dev server and deleting development files")
         shutil.rmtree(tree.build_directory)
         server_process.terminate()  # prints "Process Process-1" to sys.stderr

commit d8906997d3961ffb899df11fdc3de2bcad4b8a6c

fix: fix dev server not cleaning up on ctrl-c exit
Bug introduced in c44b7189ad06

src/cambium/cli/dev_server.py
@@ -64,10 +64,11 @@ def run_dev_server(
                     tree = TreeSpan(config.current_config)  # make new tree
                     logger.info("Re-running Cambium")
                     build(tree)
-    except Exception as e:
+    except (KeyboardInterrupt, Exception) as e:
         logger.info("Closing dev server and deleting development files")
-        shutil.rmtree(tree.build_directory)
         server_process.terminate()  # prints "Process Process-1" to sys.stderr
+        shutil.rmtree(tree.build_directory)
+
         if not isinstance(e, KeyboardInterrupt):
             logger.error(e)
             raise e