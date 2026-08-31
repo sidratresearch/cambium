c14d5fedfc221b5d2020f00289064e72995db727
Jennifer Scora <jscora@sidratresearch.com>
2026-08-31 14:33:37 -0400

    added in cambium yellow logo for dark mode and changed header in mobile

--- src/cambium/themes/root/static/js/menu.js
+++ src/cambium/themes/root/static/js/menu.js
@@ -35,30 +35,50 @@ export function attachMenuButtonListener() {
 export function themeToggle() {
   const toggle = document.getElementById("mode-toggle");
   const body = document.body;
+  const logo = document.getElementById("built-with-cambium");
+  const imageLight =
+    "Built%20With%20Cambium%20-%20Green%20on%20Transparent.svg";
+  const imageDark = "builtwithcambiumyellow.svg";
 
   toggle.addEventListener("change", function () {
     if (this.checked) {
       document.documentElement.setAttribute("data-theme", "dark");
-      body.classList.add("dark-theme");
       localStorage.setItem("theme", "dark");
       body.dataset.pfTheme = "dark";
+      // line here to change out the 'built with cambium' image
+      const basepath = getBasePathFromPath(logo.src);
+      logo.src = basepath + imageDark;
     } else {
       document.documentElement.setAttribute("data-theme", "light");
-      // body.classList.remove("dark-theme");
       localStorage.setItem("theme", "light");
       body.dataset.pfTheme = "light";
+      // line here to change out the 'built with cambium' image
+      const basepath = getBasePathFromPath(logo.src);
+      logo.src = basepath + imageLight;
     }
   });
 
   // check for saved user preference, if any, on load of the website
   document.addEventListener("DOMContentLoaded", (event) => {
-    const darkMode = localStorage.getItem("darkMode");
+    const theme = localStorage.getItem("theme");
 
-    if (darkMode === "enabled") {
-      // body.classList.add("dark-theme");
+    if (theme === "dark") {
       document.documentElement.setAttribute("data-theme", "dark");
       toggle.checked = true;
       body.dataset.pfTheme = "dark";
+      // line here to change out the 'built with cambium' image
+      const basepath = getBasePathFromPath(logo.src);
+      logo.src = basepath + imageDark;
     }
   });
+}
+
+function getBasePathFromPath(path) {
+  const lastSlash = path.lastIndexOf("/");
+  const lastBackslash = path.lastIndexOf("\\");
+  const lastSeparatorIndex = Math.max(lastSlash, lastBackslash);
+
+  return lastSeparatorIndex === -1
+    ? path
+    : path.slice(0, lastSeparatorIndex + 1);
 }
