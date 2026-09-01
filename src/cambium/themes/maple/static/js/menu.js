6e74c355ae61c881e2fd4807d90769c7c2915279
Jennifer Scora <jscora@sidratresearch.com>
2026-09-01 15:38:47 -0400

    getting the header and menu to work in desktop without duplicated header bars

--- src/cambium/themes/maple/static/js/menu.js
+++ src/cambium/themes/maple/static/js/menu.js
@@ -1,13 +1,17 @@
 function openMenu() {
-  const menuPanel = document.getElementById("menu-panel");
-  menuPanel.classList.remove("hidden");
+  const openIcon = document.getElementById("open-icon");
+  const closeIcon = document.getElementById("close-icon");
+  openIcon.classList.add("hidden");
+  closeIcon.classList.remove("hidden");
   document.addEventListener("scroll", closeMenu);
   document.addEventListener("click", closeMenuIfClickedOutside);
 }
 
 function closeMenu() {
-  const menuPanel = document.getElementById("menu-panel");
-  menuPanel.classList.add("hidden");
+  const openIcon = document.getElementById("open-icon");
+  const closeIcon = document.getElementById("close-icon");
+  openIcon.classList.remove("hidden");
+  closeIcon.classList.add("hidden");
   document.removeEventListener("scroll", closeMenu);
   document.removeEventListener("click", closeMenuIfClickedOutside);
 }
@@ -16,19 +20,18 @@ function closeMenuIfClickedOutside(event) {
   const header = document.getElementsByTagName("header")[0];
   const menuPanel = document.getElementById("menu-panel");
   if (!menuPanel.contains(event.target) & !header.contains(event.target)) {
+    menuPanel.classList.add("hidden");
     closeMenu();
   }
 }
 
 export function attachMenuButtonListener() {
-  const menuButtonOpen = document.getElementById("menu-button-open");
-  const menuButtonClose = document.getElementById("menu-button-close");
+  const menuButton = document.getElementById("menu-button");
 
-  menuButtonOpen.addEventListener("click", () => {
-    openMenu();
-  });
-  menuButtonClose.addEventListener("click", () => {
-    closeMenu();
+  menuButton.addEventListener("click", () => {
+    const menuPanel = document.getElementById("menu-panel");
+    const result = menuPanel.classList.toggle("hidden");
+    result ? closeMenu() : openMenu();
   });
 }
 
--- src/cambium/themes/root/static/js/menu.js
+++ src/cambium/themes/root/static/js/menu.js
@@ -1,13 +1,17 @@
 function openMenu() {
-  const menuPanel = document.getElementById("menu-panel");
-  menuPanel.classList.remove("hidden");
+  const openIcon = document.getElementById("open-icon");
+  const closeIcon = document.getElementById("close-icon");
+  openIcon.classList.add("hidden");
+  closeIcon.classList.remove("hidden");
   document.addEventListener("scroll", closeMenu);
   document.addEventListener("click", closeMenuIfClickedOutside);
 }
 
 function closeMenu() {
-  const menuPanel = document.getElementById("menu-panel");
-  menuPanel.classList.add("hidden");
+  const openIcon = document.getElementById("open-icon");
+  const closeIcon = document.getElementById("close-icon");
+  openIcon.classList.remove("hidden");
+  closeIcon.classList.add("hidden");
   document.removeEventListener("scroll", closeMenu);
   document.removeEventListener("click", closeMenuIfClickedOutside);
 }
@@ -16,18 +20,29 @@ function closeMenuIfClickedOutside(event) {
   const header = document.getElementsByTagName("header")[0];
   const menuPanel = document.getElementById("menu-panel");
   if (!menuPanel.contains(event.target) & !header.contains(event.target)) {
+    menuPanel.classList.add("hidden");
     closeMenu();
   }
 }
 
 export function attachMenuButtonListener() {
-  const menuButtonOpen = document.getElementById("menu-button-open");
-  const menuButtonClose = document.getElementById("menu-button-close");
+  const menuButton = document.getElementById("menu-button");
 
-  menuButtonOpen.addEventListener("click", () => {
-    openMenu();
-  });
-  menuButtonClose.addEventListener("click", () => {
-    closeMenu();
+  menuButton.addEventListener("click", () => {
+    const menuPanel = document.getElementById("menu-panel");
+    const result = menuPanel.classList.toggle("hidden");
+    result ? closeMenu() : openMenu();
   });
 }
+
+// export function attachMenuButtonListener() {
+//   const menuButtonOpen = document.getElementById("menu-button-open");
+//   const menuButtonClose = document.getElementById("menu-button-close");
+
+//   menuButtonOpen.addEventListener("click", () => {
+//     openMenu();
+//   });
+//   menuButtonClose.addEventListener("click", () => {
+//     closeMenu();
+//   });
+// }
